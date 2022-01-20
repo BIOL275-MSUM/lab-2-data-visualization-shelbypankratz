56909746d6e5eeeb873b22b3fc278ad18fc4ea0c
shelby pankratz <shelbypankratz09@gmail.com>
2022-01-20 12:54:09 -0600

    First commit

--- assignment.R
+++ assignment.R
@@ -1,13 +1,49 @@
-# load packages
+# load packages -----------------------------------------------------------
 
 library(tidyverse)
 
 library(palmerpenguins)
 penguins
 
-# graphing penguins data
+# graphing penguins data ----
 
 ggplot(data=penguins)
 
+# graph with body mass and flipper length
 ggplot(data = penguins) + 
-  geom_point(mapping = aes(x = body_mass_g, y = flipper_length_mm))
+  geom_point(mapping = aes(x = body_mass_g, 
+                           y = flipper_length_mm))
+
+# aesthetic mapping ----
+
+# give each species its own color
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = body_mass_g, 
+                           y = flipper_length_mm, 
+                           color=species))
+
+# give each species its own color, give each island its own shape 
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = body_mass_g, 
+                           y = flipper_length_mm, 
+                           color=species, 
+                           shape=island))
+
+# facets ----
+
+# put each species into its own facet
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = body_mass_g, 
+                           y = flipper_length_mm, 
+                           color=species, 
+                           shape=island)) + 
+facet_wrap(~species, nrow=1)
+
+# put each island and species into its own graph
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = body_mass_g, 
+                           y = flipper_length_mm, 
+                           color=species, 
+                           shape=island)) + 
+  facet_grid(island~species)
+
--- examples.R
+++ examples.R
@@ -3,3 +3,26 @@ library(tidyverse)
 
 # print mpg
 mpg
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy), color="blue")
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color=class))
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, size = class))
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, shape = class))
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color = class), shape=15)
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~ class, nrow=3)
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_grid(.~ cyl)
+
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = body_mass_g, y = flipper_length_mm, color=species, shape=island)) + 
+  facet_wrap(~species, nrow=1)
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = body_mass_g, y = flipper_length_mm, color=species, shape=island)) + 
+  facet_grid(island~species)
